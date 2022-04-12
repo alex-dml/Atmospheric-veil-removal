1a06a0bc92324b7d94afeb1f0da09ac95a8adaa1
Alexandra Duminil <88732189+alex-dml@users.noreply.github.com>
2022-04-12 16:47:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jul 20 12:15:52 2020
-
-@author: duminil
-"""
-
 import cv2
 import resto
-import glob
+import glob, os
 import argparse
+from evaluations import ssim_psnr
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--indir", default = "E:/Datasets/RESIDE/SOTS/SOTS/outdoor/hazy/*")
+    parser.add_argument("--indir", default = "E:/Datasets/RESIDE/SOTS/SOTS/outdoor/")
     parser.add_argument("--outdir", default = "D:/1-These/4-Images/optim/")
     parser.add_argument("--method", default = "interp_NR", help = "NR or interp_NR")
                         
@@ -26,21 +20,32 @@ def parse_arguments():
 
 def main():
     args = parse_arguments()
+    tot_ssim = 0
+    tot_psnr = 0
     
-    i = 0
-    for x in glob.glob(args.indir):
+    file = glob.glob(args.indir + 'hazy/*.jpg')
+    for x in file:
         
         fog = cv2.imread(x)/255
     
         res = resto.mfp(fog, 90, 0.05, (10,10), args.flagrefine, args.version, args.method)
         
-        
         # save results
         # cv2.imwrite(args.outdir + str(i) +'.png', res*255)
         
+        # evaluation
+        path_gt = args.indir + 'gt/' + os.path.basename(x).rsplit('_')[0] + '.png'
+        gt = cv2.imread(path_gt)/255
+        ssim, psnr = ssim_psnr(gt, res)
+        
+        tot_ssim += ssim
+        tot_psnr += psnr
+        
         # display
-        cv2.imshow('res', res)
-        cv2.waitKey(0)
+        # cv2.imshow('res', res)
+        # cv2.waitKey(0)
+        
+    print("ssim", "{0:.2f}".format(tot_ssim/len(file)), "psnr", "{0:.2f}".format(tot_psnr/len(file)))
 
 
 if __name__ == "__main__":
